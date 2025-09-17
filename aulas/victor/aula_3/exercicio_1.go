65e5ee49ed72bc0d0a6921a1fdb13ac3e4123b47
Victor Braga <victorsouzabraga14@gmail.com>
2025-09-17 10:46:12 -0300

    Novas funções de divisão e multiplicação adicionadas

--- aulas/victor/aula_3/exercicio_1.go
+++ aulas/victor/aula_3/exercicio_1.go
@@ -1,13 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	resultado, err := divisao(10, 0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(resultado)
+	}
+	{
+		resultado, err := multiplicacao(10, -5)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resultado)
+		}
 
-	fmt.Println(soma(6, 5))
+	}
+}
+
+func divisao(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("não coloque 0, animal")
+	}
+	return a / b, nil
 }
 
 func soma(a int, b int) int {
 	return a + b
+}
+
+func multiplicacao(a float64, b float64) (float64, error) {
+	resultado := a * b
+	if resultado < 0 {
+		return 0, errors.New("resultado negativo")
+	}
+	return resultado, nil
 }
